e287ca345cd0a352d1333a0060efcd8cc20c3dc6
shusaku009 <shusaku009@gmail.com>
2023-11-07 09:11:26 +0900

    add: コンポーネント自体の型定義

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import './App.css';
 import { Todo } from './Todo';
 import { TodoType } from './types/todo';
+import { Text } from './Text';
 
 function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
@@ -16,6 +17,7 @@ function App() {
   };
   return (
     <div className="App">
+      <Text color="red" fontSize="18px" />
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
         <Todo 
---
+++ src/Text.tsx
@@ -0,0 +1,11 @@
+import { VFC } from "react"
+
+type Props = {
+  color: string;
+  fontSize: string;
+}
+
+export const Text: VFC<Props> = (props: Props) => {
+  const { color, fontSize } = props;
+  return <p style={{ color, fontSize }}>テキストです</p>
+}
